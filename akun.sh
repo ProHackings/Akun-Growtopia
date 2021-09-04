eccb3eecc150372b328fcae92c8a4fa73487afb9
ProHackings <89902010+ProHackings@users.noreply.github.com>
2021-09-04 17:21:53 +0700

    Akun.sh

---
+++ akun.sh
@@ -0,0 +1,209 @@
+clear
+sleep 1
+echo "_________________"
+echo
+figlet "By Talla"
+echo
+echo "_________________"
+echo
+echo "Selamat Datang"
+echo "Subscribe :murid grunt"
+sleep 5
+echo
+echo "Pilih Menu"
+echo
+echo "(1). Script Lainnya"
+echo
+echo "(2). Akun GT Terbaru"
+echo
+echo "(3). Akun GT Fresh"
+echo
+echo "(4). Akun GT pro"
+echo
+echo "(5).Keluar"
+echo
+read -p "Ketik Nomer Menu : " lek
+
+if [[ $lek == 1 ]]
+then
+clear
+figlet "By Talla"
+echo "LIHAT YOUTUBE Hax Terminal UNTUK SCRIPT LAINNYA :)"
+fi
+
+if [[ $lek == 2 ]]
+then
+clear
+figlet "By Talla"
+echo
+echo
+echo "Grow ID : RockSejati
+Password : Penyalai
+
+Grow ID : Faris6b
+Password : Fariz12345
+
+Grow ID : mohd13xd
+Password : mohd13
+
+Grow ID : F4rhangt
+Password : Farhangaming
+
+Grow ID : Kuypalm
+Password : 123456789
+
+Grow ID : realmrtgl
+Password : geras
+
+Grow ID : Kenyot12
+Password : Kenarellano187
+
+Grow ID : Putakayo
+Password : Gago12345
+
+Grow ID : amangkecap
+Password : amangpcc
+
+Grow ID : haxtry
+Password : johan123"
+fi
+
+if [[ $lek == 3 ]]
+then
+clear
+figlet "By Talla"
+echo
+echo
+echo "Growtopia ID: Farelviper
+Password: 9876
+
+Growtopia ID: Shad0wing
+Password: Rzor
+
+Growtopia ID: AbiyuProZS
+Password: Biyu
+
+Growtopia ID: Oranggdevil1
+Password: Irondevill
+
+Growtopia ID: Agungsuper99
+Password: agung
+
+Growtopia ID: Indra66
+Password: Pensil
+
+Growtopia ID: TheBBS
+Password: LM10
+
+Growtopia ID: GT07GT
+Password: Papaku
+
+Growtopia ID: BapakPolisi
+Password: Garuda"
+fi
+
+if [[ $lek == 4 ]]
+then
+clear
+figlet "By Talla"
+echo
+echo
+echo "Grow ID : RockSejati
+Password : Penyalai
+
+Grow ID : Faris6b
+Password : Fariz12345
+
+Grow ID : mohd13xd
+Password : mohd13
+
+Grow ID : F4rhangt
+Password : Farhangaming
+
+Grow ID : Marchell23
+Password : Lim03
+
+Grow ID : Februarita11
+Password : Natasya05
+
+Grow ID : farhan876
+Password : farhanbroody
+
+Grow ID : Rizky03
+Password : 54320
+
+Grow ID : yosia1234kdkd
+Password : yosia123
+
+Grow ID : TheKillerJr
+Password : 156JKP156
+
+Grow ID : Shad0wing
+Password : Razor
+
+Grow ID : Farelviper
+Password : 9876
+
+Grow ID : Agungsuper99
+Password : agung
+
+Grow ID : Indra66
+Password : Pensil
+
+Grow ID : AbiyuProZS
+Password : Biyu
+
+Grow ID : TheBBS
+Password : LM10
+
+Grow ID : BapakPolisi
+Password : Garuda
+
+Grow ID : GT07GT
+Password : Papaku
+
+Grow ID : SukmaGirl
+Password : cantik
+
+Grow ID : yuvensius
+Password : ilusilah789
+
+Grow ID : RaffaelCBS
+Password : g1l2n5
+
+Grow ID : Marchellio
+Password : Matthew23
+
+Grow ID : selalususetia
+Password : ninewol65
+
+Grow ID : Kuypalm
+Password : 123456789
+
+Grow ID : realmrtgl
+Password : geras
+
+Grow ID : Kenyot12
+Password : Kenarellano187
+
+Grow ID : Putakayo
+Password : Gago12345
+
+Grow ID : amangkecap
+Password : amangpcc
+
+Grow ID : haxtry
+Password : johan123"
+fi
+
+if [[ $lek == 5 ]]
+then
+clear
+figlet "By Talla"
+echo
+echo
+echo "Subscribe Hax Terminal"
+echo
+echo "Ketik exit untuk keluar atau ketik sh akun.sh untuk masuk kembali :)"
+exit
+fi
